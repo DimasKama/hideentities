3345f551a3d2bf45ed45bf439601377bc3c9d5b2
Mynam <mynameisiphone10@gmail.com>
2025-06-12 02:32:09 +0500

    Update to mc1.21.3

--- common/src/main/java/ru/dimaskama/hideentities/mixin/WorldRendererMixin.java
+++ common/src/main/java/ru/dimaskama/hideentities/mixin/WorldRendererMixin.java
@@ -1,20 +1,20 @@
 package ru.dimaskama.hideentities.mixin;
 
-import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import net.minecraft.client.render.WorldRenderer;
-import net.minecraft.entity.Entity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import ru.dimaskama.hideentities.HideEntities;
-
-import java.util.List;
 
 @Mixin(WorldRenderer.class)
 abstract class WorldRendererMixin {
 
-    @ModifyExpressionValue(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/world/ClientWorld;getEntities()Ljava/lang/Iterable;"))
-    private Iterable<Entity> modifyRenderedEntities(Iterable<Entity> original) {
-        return HideEntities.areEntitiesHidden() ? List.of() : original;
+    @Inject(method = "getEntitiesToRender", at = @At("HEAD"), cancellable = true)
+    private void modifyRenderedEntities(CallbackInfoReturnable<Boolean> cir) {
+        if (HideEntities.areEntitiesHidden()) {
+            cir.setReturnValue(false);
+        }
     }
 
 }
